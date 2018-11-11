b43d57cdecc7d88fb5503f55977275b72bc6e927
rumroses12 <grace@daher.net>
2018-11-11 00:16:00 -0500

    feature(added data form component)

--- App.js
+++ App.js
@@ -7,9 +7,22 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  PushNotificationIOS,
+  TouchableOpacity,
+  TextInput,
+  Picker,
+  AsyncStorage,
+  Button
+} from "react-native";
 import { Calendar, CalendarList, Agenda } from "react-native-calendars";
-// import MonthView from "./components/MonthView";
+
+// import BackgroundTask from "react-native-background-task";
+// import queueFactory from "react-native-queue";
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
@@ -18,77 +31,291 @@ const instructions = Platform.select({
     "Shake or press menu button for dev menu"
 });
 
+//REQUEST PERMISSION TO USE PUSH NOTIFICATIONS
+PushNotificationIOS.requestPermissions();
+
+// //ADD EVENT LISTENER CALLBACK FOR WHEN NOTIFICATION IS DELIVERED- this doesn't work unless the applicaiton is running.
+// PushNotificationIOS.addEventListener("localNotification", eventCallback);
+// let alerts = 0;
+
+const userInfo2 = { alert: "alert2" };
+
+//CANCEL FIRST NOTIFICATION
+
+// while (alerts < 10) {
+//   setTimeout(() => {
+//     PushNotificationIOS.presentLocalNotification({
+//       alertBody: "hello I am alertBody"
+//     });
+//     alerts++;
+//   }, interval);
+// }
+
+_storeData = async data => {
+  try {
+    await AsyncStorage.setItem("CalendarAlarm", data);
+    console.log("store data was run");
+  } catch (error) {
+    // Error saving data
+  }
+};
+
+_retrieveData = async key => {
+  try {
+    console.log("inside retrieve data");
+    const value = await AsyncStorage.getItem(key);
+    if (value !== null) {
+      // We have data!!
+      console.log("data", value);
+      return value;
+    }
+  } catch (error) {
+    // Error retrieving data
+  }
+};
+
+_storeData("{}");
+
 type Props = {};
 export default class App extends Component<Props> {
+  constructor() {
+    super();
+
+    //I had to put month/day/year etc. directly onto state in stead of an object. If you put them
+    //inside a larger object, you cannot change a picker without the others resetting.
+    this.state = {
+      title: "",
+      month: "1",
+      day: "1",
+      year: "2017",
+      hour: "1",
+      minute: "0",
+      AmPm: "AM"
+    };
+  }
+  componentWillMount() {
+    PushNotificationIOS.getDeliveredNotifications(notifications => {
+      const ids = notifications.map(elem => elem.identifier);
+      PushNotificationIOS.removeDeliveredNotifications([...ids.slice(1)]);
+      console.log(notifications);
+      if (notifications.length > 0) {
+        PushNotificationIOS.cancelLocalNotifications(notifications[0].userInfo);
+      }
+    });
+  }
+  submit = async () => {
+    PushNotificationIOS.scheduleLocalNotification({
+      fireDate: Date.now() + 60000,
+      alertBody: "hello I am alertBody #2",
+      repeatInterval: "minute",
+      userInfo: userInfo2
+    });
+    let data = await _retrieveData("CalendarAlarm");
+    // prettier-ignore
+    const key = `${this.state.month}-${this.state.day}-${this.state.year}-${this.state.hour}-${this.state.minute}}`;
+
+    const dataSubmission = JSON.parse(data);
+    dataSubmission[key] = this.state;
+    console.log("dataSubmission", dataSubmission);
+    const stringDataSubmission = JSON.stringify(dataSubmission);
+    await _storeData(stringDataSubmission);
+    let val = await _retrieveData("CalendarAlarm");
+    //SCHEDULE  SECOND NOTIFICATION TO REPEAT
+  };
   render() {
-    console.log("hello");
     return (
       // <View style={styles.container}>
       //   <Text style={styles.welcome}>Welcome to React Native!</Text>
       //   <Text style={styles.instructions}>To get started, edit App.js</Text>
       //   <Text style={styles.instructions}>{instructions}</Text>
       // </View>
-      //
-      <Calendar
-        // Initially visible month. Default = Date()
-        current={"2018-03-01"}
-        // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-        minDate={"2018-05-10"}
-        // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-        maxDate={"2018-05-30"}
-        // Handler which gets executed on day press. Default = undefined
-        onDayPress={day => {
-          console.log("selected day", day);
-        }}
-        // Handler which gets executed on day long press. Default = undefined
-        onDayLongPress={day => {
-          console.log("selected day", day);
-        }}
-        // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-        monthFormat={"yyyy MM"}
-        // Handler which gets executed when visible month changes in calendar. Default = undefined
-        onMonthChange={month => {
-          console.log("month changed", month);
-        }}
-        // Hide month navigation arrows. Default = false
-
-        // Replace default arrows with custom ones (direction can be 'left' or 'right')
-
-        // Do not show days of other months in month page. Default = false
-        hideExtraDays={true}
-        // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
-        // day from another month that is visible in calendar page. Default = false
-        disableMonthChange={true}
-        // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
-        firstDay={1}
-        // Hide day names. Default = false
-        hideDayNames={true}
-        // Show week numbers to the left. Default = false
-        showWeekNumbers={true}
-        // Handler which gets executed when press arrow icon left. It receive a callback can go back month
-        onPressArrowLeft={substractMonth => substractMonth()}
-        // Handler which gets executed when press arrow icon left. It receive a callback can go next month
-        onPressArrowRight={addMonth => addMonth()}
-      />
+      <View style={styles.container}>
+        <View style={styles.picker} key={1}>
+          <Text>Title:</Text>
+          <TextInput
+            style={{
+              height: 50,
+              width: 200,
+              borderColor: "gray",
+              borderWidth: 1
+            }}
+          />
+        </View>
+        <View style={styles.picker} key={2}>
+          <Text>Month:</Text>
+          <Picker
+            selectedValue={this.state.month}
+            itemStyle={{ height: 100, width: 50 }}
+            style={{
+              height: 100,
+              width: 50,
+              padding: 5,
+              margins: 0
+            }}
+            onValueChange={(itemValue, itemIndex) =>
+              this.setState({ month: itemValue })
+            }
+          >
+            {Array(12)
+              .fill()
+              .map((elem, idx) => idx + 1)
+              .map(number => {
+                return (
+                  <Picker.Item
+                    key={number}
+                    label={"" + number}
+                    value={"" + number}
+                  />
+                );
+              })}
+          </Picker>
+        </View>
+        <View style={styles.picker} key={3}>
+          <Text>Day:</Text>
+          <Picker
+            selectedValue={this.state.day}
+            itemStyle={{ height: 100, width: 50 }}
+            style={{
+              height: 100,
+              width: 50,
+              padding: 5,
+              margins: 0
+            }}
+            onValueChange={(itemValue, itemIndex) =>
+              this.setState({ day: itemValue })
+            }
+          >
+            {Array(31)
+              .fill()
+              .map((elem, idx) => idx + 1)
+              .map(number => {
+                return <Picker.Item label={"" + number} value={"" + number} />;
+              })}
+          </Picker>
+        </View>
+        <View style={styles.picker} key={4}>
+          <Text> Year: </Text>
+          <Picker
+            selectedValue={this.state.year}
+            itemStyle={{ height: 100, width: 80 }}
+            style={{
+              height: 100,
+              width: 80,
+              padding: 5,
+              margins: 0
+            }}
+            onValueChange={(itemValue, itemIndex) =>
+              this.setState({ year: itemValue })
+            }
+          >
+            {Array(30)
+              .fill(new Date().getFullYear() - 1)
+              .map((elem, idx) => idx + elem)
+              .map(number => {
+                return <Picker.Item label={"" + number} value={"" + number} />;
+              })}
+          </Picker>
+        </View>
+        <View style={styles.picker} key={5}>
+          <Text> Time: </Text>
+          <Picker
+            selectedValue={this.state.hour}
+            itemStyle={{ height: 100, width: 80 }}
+            style={{
+              height: 100,
+              width: 80,
+              padding: 5,
+              margins: 0
+            }}
+            onValueChange={(itemValue, itemIndex) => {
+              this.setState({ hour: itemValue + "" });
+            }}
+          >
+            {Array(12)
+              .fill()
+              .map((elem, idx) => idx + 1)
+              .map(number => {
+                return (
+                  <Picker.Item
+                    key={number}
+                    label={"" + number}
+                    value={"" + number}
+                  />
+                );
+              })}
+          </Picker>
+          <Text> : </Text>
+          <Picker
+            selectedValue={this.state.minute}
+            itemStyle={{ height: 100, width: 80 }}
+            style={{
+              height: 100,
+              width: 80,
+              padding: 5,
+              margins: 0
+            }}
+            onValueChange={(itemValue, itemIndex) => {
+              this.setState({ minute: itemValue });
+              console.log(
+                "minutes",
+                this.state.minute,
+                "itemValue",
+                itemValue,
+                this.state.AmPm
+              );
+            }}
+          >
+            {Array(12)
+              .fill()
+              .map((elem, idx) => idx * 5)
+              .map(number => {
+                return <Picker.Item label={"" + number} value={"" + number} />;
+              })}
+          </Picker>
+          <Picker
+            selectedValue={this.state.AmPm}
+            itemStyle={{ height: 100, width: 80 }}
+            style={{
+              height: 100,
+              width: 80,
+              padding: 5,
+              margins: 0
+            }}
+            onValueChange={(itemValue, itemIndex) => {
+              this.setState({ AmPm: itemValue });
+            }}
+          >
+            <Picker.Item label="AM" value="AM" />
+            <Picker.Item label="PM" value="PM" />
+          </Picker>
+        </View>
+        <Button onPress={this.submit} title="Remind Me" color="#841584" />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   justifyContent: "center",
-  //   alignItems: "center",
-  //   backgroundColor: "#F5FCFF"
-  // },
-  // welcome: {
-  //   fontSize: 20,
-  //   textAlign: "center",
-  //   margin: 10
-  // },
-  // instructions: {
-  //   textAlign: "center",
-  //   color: "#333333",
-  //   marginBottom: 5
-  // }
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "#F5FCFF",
+    padding: 20
+  },
+  picker: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    textAlign: "center"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
